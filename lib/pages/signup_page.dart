4e113b1f1a80a448c07d657935779c2ff690ae86
Sumin Hwang <hhy9706@pusan.ac.kr>
2025-05-10 20:34:59 +0900

    [FEAT] 회원가입 페이지 구현 #1

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,11 +1,246 @@
 import 'package:flutter/material.dart';
+//상의 후 위젯 추가
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
 
-@override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }}
+  @override
+  SignUpPageState createState() => SignUpPageState();
+}
 
+class SignUpPageState extends State<SignUpPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+
+  String username = '';
+  String email = '';
+  String password = '';
+  String confirmPassword = '';
+
+  void sendEmailVerification(String email) {
+    // 이메일 인증 연결
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          '인증 메일이 $email 로 전송되었습니다.',
+          style: TextStyle(fontSize: 18),
+        ),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "회원가입",
+                style: TextStyle(fontSize: 31, fontWeight: FontWeight.w900),
+              ),
+              SizedBox(height: 15),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: '아이디',
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 11,
+                    ),
+                    isDense: true,
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '아이디를 입력하세요';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    username = value!;
+                  },
+                ),
+              ),
+              SizedBox(height: 25),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: '비밀번호',
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 11,
+                    ),
+                    isDense: true,
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '비밀번호를 입력하세요';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    password = value!;
+                  },
+                ),
+              ),
+              SizedBox(height: 25),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: '비밀번호 확인',
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 11,
+                    ),
+                    isDense: true,
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '비밀번호 확인을 입력하세요';
+                    }
+                    if (value != password) {
+                      return '비밀번호가 일치하지 않습니다';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    confirmPassword = value!;
+                  },
+                ),
+              ),
+              SizedBox(height: 25),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              hintText: '이메일',
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 11,
+                              ),
+                              isDense: true,
+                              border: OutlineInputBorder(),
+                              errorStyle: TextStyle(height: 0.8),
+                              helperText: ' ',
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return '이메일을 입력하세요';
+                              }
+                              if (!value.contains('@')) {
+                                return '올바른 이메일 형식이 아닙니다';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              email = value!;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          final enteredEmail = emailController.text.trim();
+                          if (enteredEmail.isEmpty ||
+                              !enteredEmail.contains('@')) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  '올바른 이메일을 입력하세요',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            );
+                            return;
+                          }
+                          sendEmailVerification(enteredEmail);
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 11,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Text(
+                          '인증',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 70),
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                      //회원가입 백엔드 연동
+                    }
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    '회원가입',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
