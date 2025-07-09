47567bc7b3cb3d63263c6e1ed4f52efbe5f2c549
Sumin Hwang <hhy9706@pusan.ac.kr>
2025-07-09 20:28:48 +0900

    [FEAT] main 추가 #13

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'routes.dart';
+import 'routes.dart'; 
 
 void main() {
   runApp(MyApp());
@@ -10,10 +10,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //시작 화면 경로
-      initialRoute: '/initial',
-      //루트 연결
-      routes: appRoutes,
+      title: 'My App',
+      initialRoute: '/login', 
+      routes: routes,
     );
   }
 }
