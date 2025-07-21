2d2c993a8e2246933cb0bf98dd92036f50ec2a69
JIEUN <162803772+phlox22@users.noreply.github.com>
2025-07-22 02:36:03 +0900

    Feat : 닉네임 변경 페이지 생성

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '/api/http_client.dart';
 import '/pages/password_reset_new_page.dart'; // 직접 push 방식이므로 import 필요
 import 'package:resq_frontend/pages/login_user_changePWD_page.dart'; // 경로를 프로젝트의 정확한 위치로 맞춰야 함
+import 'package:resq_frontend/pages/change_nickname_page.dart'; // 경로 확인
 
 
 class UserProfilePage extends StatefulWidget {
@@ -63,62 +64,14 @@ class _UserProfilePageState extends State<UserProfilePage> {
   }
 
   void _onChangeNickname() {
-    final TextEditingController nicknameController = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('닉네임 변경'),
-        content: TextField(
-          controller: nicknameController,
-          decoration: const InputDecoration(
-            hintText: '새 닉네임 입력',
-          ),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('취소'),
-          ),
-          TextButton(
-            onPressed: () async {
-              String newNickname = nicknameController.text.trim();
-              if (newNickname.isEmpty) return;
-
-              const storage = FlutterSecureStorage();
-              final token = await storage.read(key: 'accessToken');
-
-              if (token == null) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('로그인이 필요합니다')),
-                );
-                return;
-              }
-
-              final response = await HttpClient.patchUserUpdate(
-                token: token,
-                data: {"username": newNickname},
-              );
-
-              Navigator.pop(context);
-
-              if (response['success']) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('닉네임이 변경되었습니다')),
-                );
-                fetchUserInfo(); // 변경 후 정보 다시 로드
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(response['message'] ?? '닉네임 변경 실패')),
-                );
-              }
-            },
-            child: const Text('확인'),
-          ),
-        ],
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const ChangeNicknamePage(),
       ),
     );
   }
+
 
 
   void _onChangePassword() {
