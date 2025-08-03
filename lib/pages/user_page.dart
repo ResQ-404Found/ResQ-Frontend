5a0f7f8980f793c2c86e104eec6c4804ab425f04
JIEUN <162803772+phlox22@users.noreply.github.com>
2025-08-03 18:24:50 +0900

    Fix : 마이페이지 뒤로가기 없앰

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -133,6 +133,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
     return Scaffold(
       appBar: AppBar(
         scrolledUnderElevation: 0,
+        automaticallyImplyLeading: false,
         leading: null,
         title: const Text(
           '마이페이지',
